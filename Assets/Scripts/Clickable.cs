49a94d387a6491dad2039d3116d545536989341c
s <sylwia.kucharska11@gmail.com>
2024-02-28 12:56:17 +0100

    Refactor OnPointerClick function.

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -19,44 +19,53 @@ public class Clickable : MonoBehaviour, IPointerClickHandler
     {
         if (!isReversed)
         {
-            // revrese card and update game state
             if (Managers.GameState.reversed_not_matched_cards.Count <2)
             {
-                isReversed = !isReversed;
-                Managers.GameState.reversed_cards += 1;
-                Managers.GameState.reversed_not_matched_cards.Add(this);
-                Debug.Log(name + " reversed: " + isReversed);
-                Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
+                reverseCard();
             }
 
-            // deal with cards if 2 are revresed
             if (Managers.GameState.reversed_not_matched_cards.Count == 2)
             {
                 Clickable card_reveresed_1 = Managers.GameState.reversed_not_matched_cards[0];
                 Clickable card_reveresed_2 = Managers.GameState.reversed_not_matched_cards[1];
                 if (card_reveresed_1.cardImage == card_reveresed_2.cardImage)
                 {
-                    card_reveresed_1.isMatched = true;
-                    card_reveresed_2.isMatched = true;
-                    Managers.GameState.matched_cards += 2;
-                    Managers.GameState.reversed_not_matched_cards.Clear();
-                    Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
-                    Debug.Log("Managers state for reversed cards:" + Managers.GameState.matched_cards);
+                    markPairAsMatched(card_reveresed_1, card_reveresed_2);
                 }
                 else {
-                    // # wait 3 sec and revert to the previous state
                     DOTween.Sequence().AppendInterval(2.5f).OnComplete(
                         ()=>{
-                            Managers.GameState.reversed_not_matched_cards.Clear();
-                            Managers.GameState.reversed_cards -= 2;
-                            card_reveresed_1.isReversed = false;
-                            card_reveresed_2.isReversed = false;
-                            Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
-                            Debug.Log("Managers state for reversed cards:" + Managers.GameState.matched_cards);
+                            revertCardsToPreviousState(card_reveresed_1, card_reveresed_2);
                         }
                     );
                 }
             }
         }
+    }
+
+    public void reverseCard(){
+        this.isReversed = !this.isReversed;
+        Managers.GameState.reversed_cards += 1;
+        Managers.GameState.reversed_not_matched_cards.Add(this);
+        Debug.Log(this.name + " reversed: " + this.isReversed);
+        Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
+    }
+
+    public void markPairAsMatched(Clickable card_reveresed_1, Clickable card_reveresed_2){
+        card_reveresed_1.isMatched = true;
+        card_reveresed_2.isMatched = true;
+        Managers.GameState.matched_cards += 2;
+        Managers.GameState.reversed_not_matched_cards.Clear();
+        Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
+        Debug.Log("Managers state for reversed cards:" + Managers.GameState.matched_cards);
+    }
+
+    public void revertCardsToPreviousState(Clickable card_reveresed_1, Clickable card_reveresed_2) {
+        Managers.GameState.reversed_not_matched_cards.Clear();
+        Managers.GameState.reversed_cards -= 2;
+        card_reveresed_1.isReversed = false;
+        card_reveresed_2.isReversed = false;
+        Debug.Log("Managers state for reversed cards:" + Managers.GameState.reversed_cards);
+        Debug.Log("Managers state for reversed cards:" + Managers.GameState.matched_cards);
     }
 }
